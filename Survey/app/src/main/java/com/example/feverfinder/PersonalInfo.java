876fea6b76817f7c1f2988a47434499e8a14ca2c
Charlie Maclean <c-h-a-r-l-i-e@users.noreply.github.com>
2019-02-05 18:35:57 +0000

    Add some survey menu content

--- Survey/app/src/main/java/com/example/feverfinder/PersonalInfo.java
+++ Survey/app/src/main/java/com/example/feverfinder/PersonalInfo.java
@@ -1,7 +1,9 @@
 package com.example.feverfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PersonalInfo extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class PersonalInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_info);
+    }
+
+    public void onSubmit(View view) {
+        //TODO: actually store the data
+
+        Intent intent = new Intent(this, SurveyStatus.class);
+        startActivity(intent);
     }
 }
