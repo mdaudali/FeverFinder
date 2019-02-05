60e26199caf96ac05a7e2d594f43bc6eda2a96a0
Peti <pmernyei@gmail.com>
2019-02-05 15:37:00 +0000

    Dashboard example used as basis for navigation stuff

---
+++ Dashboard/src/exampleData.js
@@ -0,0 +1,19 @@
+export const examplePeople = [
+  {
+    name: "John Doe",
+    age: 23,
+    moreInfo: "just noting we'll prob have a bunch of more specific fields"
+  },
+
+  {
+    name: "Person Guy",
+    age: 101,
+    moreInfo: "I don't intend a field with this name to actually be a thing"
+  },
+
+  {
+    name: "Random Someone",
+    age: 10,
+    moreInfo: "lorem ipsum stuff things etc"
+  }
+];
