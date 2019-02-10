67e3beb8ed7b6b0c22c8d26f1cea456cff45d161
Sumaiyah Kola <sumaiyahkola1@gmail.com>
2019-02-10 15:12:40 +0000

    Added a way to read in questions/answers into a map

--- Survey/app/src/main/java/com/example/feverfinder/questions/Response.java
+++ Survey/app/src/main/java/com/example/feverfinder/questions/Response.java
@@ -3,7 +3,7 @@ package com.example.feverfinder.questions;
 import java.util.List;
 
 /* All questions will have a type
- * Questions with a type of the form select_... will also have a list of options */
+ * Questions with a type of the form select_... will have a list of options */
 public class Response {
     String type;
     List<Option> options;
