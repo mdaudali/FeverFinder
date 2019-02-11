7f3f24216f90dbf68dc38e2c07119fbf7fa69929
Mohammed Daudali <mdaudali786@gmail.com>
2019-02-11 16:20:53 +0000

    Separated details panel and personpanel, and dynamically generates panels of objects

--- Dashboard/src/detailspanel.js
+++ Dashboard/src/detailspanel.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import classNames from 'classnames';
 import Typography from '@material-ui/core/Typography';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel'
+import ExpansionPanelSummary from "@material-ui/core/es/ExpansionPanelSummary/ExpansionPanelSummary";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
+import {CardContent, ExpansionPanelDetails, withStyles} from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import PersonPanel from "./personpanel";
+
+
 
 class DetailsPanel extends React.Component {
   render() {
-    return <span> This is the details panel! </span>
+    const personObj = {
+          name: "Mohammed Daudali",
+          age: 18,
+          other: {
+            "Hi": "Mohammed",
+            orderedValues: ["Hi", "hello"]
+          },
+          orderedValues: ["name", "age", "other"]
+        };
+    return (
+        <PersonPanel person={personObj}/>
+  )
   }
 }
 
 export default DetailsPanel;
+
