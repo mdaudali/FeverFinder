5bd053fa2bc6215ad14f677615a2515b9bd99714
Sumaiyah Kola <sumaiyahkola1@gmail.com>
2019-02-15 09:22:20 +0000

    Deleted old 'relevant' method from Question.java

--- Survey/app/src/main/java/com/example/feverfinder/questions/Question.java
+++ Survey/app/src/main/java/com/example/feverfinder/questions/Question.java
@@ -38,51 +38,6 @@ abstract public class Question implements Serializable, SelectionChangedListener
         return relevant;
     }
 
-    public boolean waitUntilRelevant(List<Question> currentQuestions){
-        // in the form ${<parentName>} = '<parentValue>' or selected(${<parentName>}, '<parentValue>')
-        // ${<parentName>} = <parentName> is select_one
-        // selected(${<parentName>}, '3') = <parentName> is select_multiple
-
-        // name of the question q depends on
-        String parentName = ""; String parentValue = "";
-
-        // Pattern Matching over strings in relevant
-        Pattern p; Matcher m;
-        if (getRelevant().startsWith("$")){ // select_one
-            p = Pattern.compile("\\$\\{([^}]+)\\} ?\\= ?\\'([^{]+)\\'"); // to match e.g. ${see_dump}='1'
-            m = p.matcher(getRelevant());
-            if (m.find()) {
-                parentName = m.group(1);
-                parentValue = m.group(2);
-            }
-        } else { // select multiple
-            p = Pattern.compile("selected\\(\\$\\{([^}]+)\\} ?\\, ?\\'([^{]+)\\'\\)"); // to match e.g. selected(${contact}, '7')
-            m = p.matcher(getRelevant());
-            if (m.find()){
-                parentName = m.group(1);
-                parentValue = m.group(2);
-            }
-        }
-
-        // question q depends on is always a SelectQuestion
-        SelectQuestion parentSelectQuestion = null;
-
-        // find question that q depends on
-        for (Question parentQuestion: currentQuestions){
-            if (parentQuestion.getName().equals(parentName)){
-                parentSelectQuestion = (SelectQuestion) parentQuestion;
-                break;
-            }
-        }
-        // see if answer of question allows q to be visible
-        for (Option o: parentSelectQuestion.getSelected()){
-            if (o.getName().equals(parentValue)){
-                return true;
-            }
-        }
-        return false;
-    }
-
     /**
      * This method returns true if the question is relevant and should be displayed
      *
