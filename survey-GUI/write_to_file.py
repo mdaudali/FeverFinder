59725424195abb3d4003fd8513234d56eccd97fc
Sumaiyah Kola <sumaiyahkola1@gmail.com>
2019-02-26 20:16:21 +0000

    updated survey questions to match database

--- survey-GUI/write_to_file.py
+++ survey-GUI/write_to_file.py
@@ -6,7 +6,6 @@ def writeQuestions(filename, question_rows):
 
     # add questions to survey sheet
     survey_sheet = wb.get_sheet_by_name(wb.get_sheet_names()[0])
-    survey_sheet.insert_rows(7)
 
     n_rows = survey_sheet.max_row + 1  # start on next empty row
     n_cols = survey_sheet.max_column
