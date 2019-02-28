2ec849b74a61f33c175db22dac75e3cb5f87aabf
Sumaiyah Kola <sumaiyahkola1@gmail.com>
2019-02-28 20:41:28 +0000

    get gps loc via button

---
+++ Survey/app/src/main/java/com/example/feverfinder/questions/GPSLoc.java
@@ -0,0 +1,28 @@
+package com.example.feverfinder.questions;
+
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Toast;
+
+import static android.support.v4.content.ContextCompat.startActivity;
+
+public class GPSLoc implements LocationListener {
+    public void onLocationChanged(Location loc) {
+        String message = String.format(
+                "New Location \n Longitude: %1$s \n Latitude: %2$s",
+                loc.getLongitude(), loc.getLatitude()
+        );
+        System.out.println(message);
+    }
+    public void onProviderDisabled(String arg0) {
+    }
+
+    public void onProviderEnabled(String provider) {
+    }
+
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+}
