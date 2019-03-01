ee9dc051cabc2a5da74407a35d1977a97dd094d7
Sumaiyah Kola <sumaiyahkola1@gmail.com>
2019-03-01 13:07:57 +0000

    update questions to add GPS type

--- survey-GUI/convert_to_JSON.py
+++ survey-GUI/convert_to_JSON.py
@@ -1,7 +1,6 @@
 from collections import OrderedDict
 import simplejson as json
 
-# TODO CHANGE FILE PATH TO ROUTE JSON TO CORRECT PLACE
 import xlrd
 
 
@@ -69,7 +68,7 @@ def convertChoicesToJSON(filename):
     j = json.dumps(choices_list)
 
     # Write to file
-    with open('files/choices.json', 'w') as f:
+    with open('../Survey/app/src/main/res/raw/choices.json', 'w') as f:
         f.write(j)
 
 def convertQuestionsToJSON(filename):
@@ -232,5 +231,5 @@ def writeToFile(sectionList):
     j = json.dumps(sectionList)
 
     # Write to file
-    with open('files/questions.json', 'w') as f:
+    with open('../Survey/app/src/main/res/raw/questions.json', 'w') as f:
         f.write(j)
